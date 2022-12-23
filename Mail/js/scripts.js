d4fe3ebd7750ea091aa2221ec408edf25284a920
Roberto Larivera <robylariv@gmail.com>
2022-12-23 08:20:58 +0100

    Aggiunta di commenti

--- Mail/js/scripts.js
+++ Mail/js/scripts.js
@@ -1,6 +1,6 @@
 console.log('int ok')
 
-// console.log('emailLIst', emailList, typeof emailList);
+// console.log('emailLIst', emailList, typeof emailList);   //debug
 
 const emailList = ['bello','prova','gino@email.it','alice@email.it','giorgio@email.it','roberto@email.it','ciao'];
 let emailInput = prompt('Inserisci la tua email');
@@ -8,21 +8,21 @@ let emailInput = prompt('Inserisci la tua email');
 
 let messaggio;
 for(let i = 0 ; i < emailList.length ; i++ ){
-    //console.log(i);
-    //console.log(emailList[i]);
+    //console.log(i);   //debug
+    //console.log(emailList[i]);   //debug
     if(emailInput == emailList[i]){
         messaggio = 'funziona';
         i = emailList.length;
-        //console.log(messaggio);
-        //console.log('messaggio finale',messaggio);
+        //console.log(messaggio);   //debug
+        //console.log('messaggio finale',messaggio);   //debug
     }
-    else if(emailInput != emailList[i]){
+    else if(emailInput != emailList[i]){        //funziona anche con solo else, ma per sicurezza mantengo else if con la condizione
         messaggio = 'non funziona';
-        //console.log(messaggio);
-        //console.log('messaggio finale',messaggio);
+        //console.log(messaggio);   //debug
+        //console.log('messaggio finale',messaggio);   //debug
     }
 }
-//console.log('fuori for',emailList[i]);
+//console.log('fuori for',emailList[i]);   //debug
 console.log('messaggio finale',messaggio);
 
 
