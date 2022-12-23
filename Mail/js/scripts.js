3952759577e75d6ef9222a7d578635c7dba77543
Roberto Larivera <robylariv@gmail.com>
2022-12-23 08:17:41 +0100

    aggiornato email

--- Mail/js/scripts.js
+++ Mail/js/scripts.js
@@ -2,23 +2,28 @@ console.log('int ok')
 
 // console.log('emailLIst', emailList, typeof emailList);
 
-const emailList = ['gino@email.it','alice@email.it','giorgio@email.it','roberto@email.it','ciao'];
+const emailList = ['bello','prova','gino@email.it','alice@email.it','giorgio@email.it','roberto@email.it','ciao'];
 let emailInput = prompt('Inserisci la tua email');
 
 
 let messaggio;
 for(let i = 0 ; i < emailList.length ; i++ ){
-    //console.log(i)
-    //console.log(emailList[i])
+    //console.log(i);
+    //console.log(emailList[i]);
     if(emailInput == emailList[i]){
-        messaggio = 'funziona'
+        messaggio = 'funziona';
+        i = emailList.length;
+        //console.log(messaggio);
+        //console.log('messaggio finale',messaggio);
     }
-    else if(emailInput == emailList.length){
-        messaggio = 'non funziona'
+    else if(emailInput != emailList[i]){
+        messaggio = 'non funziona';
+        //console.log(messaggio);
+        //console.log('messaggio finale',messaggio);
     }
 }
 //console.log('fuori for',emailList[i]);
-console.log(messaggio);
+console.log('messaggio finale',messaggio);
 
 
 
